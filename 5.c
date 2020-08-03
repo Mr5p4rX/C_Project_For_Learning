ed76c5ef9bdfea86ba62e8c0cb899badcfa7f729
Mr5p4rX <69164181+Mr5p4rX@users.noreply.github.com>
2020-08-03 23:54:48 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c;
+    printf("Enter Your 1st Number:\n");
+    scanf("%d", &a);
+    printf("Enter Your 2nd Number:\n");
+    scanf("%d", &b);
+    c = a/b;
+    printf("The Quotient is %d:\n", c);
+    return 0;
+}
